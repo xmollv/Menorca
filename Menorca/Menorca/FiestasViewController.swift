d361a6cf13fa76e38a393ae195feb3da4e2def99
Xavi Moll <xmollv@gmail.com>
2017-06-24 22:34:33 +0200

    Added a UICollectionView for the list of fiestas

--- Menorca/Menorca/FiestasViewController.swift
+++ Menorca/Menorca/FiestasViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class FiestasViewController: UIViewController {
-
+    
+    @IBOutlet private var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
+}
+
+extension FiestasViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "FiestasCell", for: indexPath)
+    }
+}
+
+extension FiestasViewController: UICollectionViewDelegate {
+    
+}
+
+extension FiestasViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 150)
+    }
 }
