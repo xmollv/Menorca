d4c49b0c032b2a7406ca547f6f02c5812563e6cf
Xavi Moll <xmollv@gmail.com>
2017-06-25 15:49:56 +0200

    Added the basic UI for the Timeline

--- Menorca/Menorca/TimelineViewController.swift
+++ Menorca/Menorca/TimelineViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 60
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "TimelineCell", for: indexPath)
+    }
 }
