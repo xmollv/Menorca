d64c9074d37df30273cda4018c5c9b2270fbe281
Xavi Moll <xmollv@gmail.com>
2017-07-23 20:02:50 +0200

    Added the codable implementation for the location

--- Menorca/Menorca/Beach.swift
+++ Menorca/Menorca/Beach.swift
@@ -7,10 +7,36 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Beach: Codable {
     let name: String
-    let latitude: String
-    let longitude: String
     let belongsTo: String
+    let latitude: Double
+    let longitude: Double
+    let location: CLLocationCoordinate2D
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case latitude
+        case longitude
+        case belongsTo
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        belongsTo = try container.decode(String.self, forKey: .belongsTo)
+        latitude = try container.decode(Double.self, forKey: .latitude)
+        longitude = try container.decode(Double.self, forKey: .longitude)
+        location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(belongsTo, forKey: .belongsTo)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
 }
